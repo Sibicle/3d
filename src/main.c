640679e1ae3fe1891771f81b16f8d6254d7b30e2
Jason N <me@sibicle.com>
2021-01-21 15:43:35 -0700

    actually fullscreen

--- src/main.c
+++ src/main.c
@@ -21,6 +21,12 @@ bool initialize_window(void) {
     return false;
   }
 
+  SDL_DisplayMode display_mode;
+  SDL_GetCurrentDisplayMode(0, &display_mode);
+
+  window_width = display_mode.w;
+  window_height = display_mode.h;
+
   window = SDL_CreateWindow(
     NULL,
     SDL_WINDOWPOS_CENTERED,
@@ -39,6 +45,8 @@ bool initialize_window(void) {
     fprintf(stderr, "error creating SDL renderer");
     return false;
   }
+
+  SDL_SetWindowFullscreen(window, SDL_WINDOW_FULLSCREEN);
 
   return true;
 }
